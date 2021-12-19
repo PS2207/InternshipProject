5252b5c48fda9f7c3ac6c479f5d7d007c8e6c748
PS2207 <pragyasinha2207@gmail.com>
2021-12-19 14:18:56 +0530

    done

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="footerStyle">
+            <h6>Copyright @ 2021 Yukti Software Designed by Wit Source Technology</h6>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Student from "./student";
+
+function Home() {
+  return (
+    <div className="homeStyle">
+      <div className="homeTableStyle">
+        <Student name="Pragya" marks="91" mobile="+91 123452340" />
+        <Student name="Komal" marks="92" mobile="+91 1234567890" />
+        <Student name="Sandhya" marks="92" mobile="+91 654789123" />
+      </div>
+      <div className="homeFormStyle">
+        <Student name="Pragya" marks="91" mobile="+91 123452340" />
+        <Student name="Komal" marks="92" mobile="+91 1234567890" />
+        <Student name="Sandhya" marks="92" mobile="+91 654789123" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
